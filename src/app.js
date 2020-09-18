e6b5ab581afc1cac0c1738a3a50f3e7f569f05fa
nghia.trungnguyen1998 <nghia.trungnguyen1998@gmail.com>
2020-09-18 19:40:10 +0700

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicDirectory = path.join(__dirname, "../public");
@@ -47,27 +48,30 @@ app.get("/weather", (req, res) => {
       error: "Address must be provided",
     });
   }
-  geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
-    if (error) {
-      return res.send({
-        error,
-      });
-    }
-
-    forecast(latitude, longitude, (error, forecast) => {
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
       if (error) {
         return res.send({
           error,
         });
       }
 
-      res.send({
-        forecast,
-        location,
-        address: req.query.address,
+      forecast(latitude, longitude, (error, forecast) => {
+        if (error) {
+          return res.send({
+            error,
+          });
+        }
+
+        res.send({
+          forecast,
+          location,
+          address: req.query.address,
+        });
       });
-    });
-  });
+    }
+  );
 });
 
 app.get("/products", (req, res) => {
@@ -98,6 +102,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000 !!!");
+app.listen(port, () => {
+  console.log(`Server is up on port ${port} !!!`);
 });
